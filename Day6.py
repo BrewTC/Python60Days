2f08437d896784f9b0c1b5f223d0ab79230ee0aa
BrewTC <73727229+BrewTC@users.noreply.github.com>
2021-01-26 15:33:57 +0800

    Update Day6.py

--- Day6.py
+++ Day6.py
@@ -2,11 +2,9 @@ import numpy as np
 # 1. 將下兩列 array 存成 npz 檔 
 array1 = np.array(range(30))
 array2 = np.array([2,3,5])
-
-with open('Day6.npz', 'wb') as f:
-    np.savez(f, array1, array2)
+np.savez('Day6.npz', array1, array2)
 
 # 2. 讀取剛剛的 npz 檔，加入下列 array 一起存成新的npz檔
-npzfile = np.load('Day6.npz')
-print(type(npzfile))
-print(npzfile.files)
+load_array = np.load('Day6.npz')
+array3 = np.array([[4,5,6],[1,2,3]])
+np.savez('new_array.npz', load_array['arr_0'], load_array['arr_1'], array3)
