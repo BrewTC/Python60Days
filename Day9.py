40e3b04c69a240e11de1592fb070c9ef154fcc3e
BrewTC <73727229+BrewTC@users.noreply.github.com>
2021-02-01 09:02:17 +0800

    Update Day9.py

--- Day9.py
+++ Day9.py
@@ -3,8 +3,7 @@ import pandas as pd
 new_boston = pd.read_csv('boston.csv', usecols=['CHAS', 'NOX', 'RM'])
 # print(new_boston)
 
-# xlsx_boston = pd.read_excel('new_boston.csv')
-# print(xlsx_boston)
-
-# new_boston = new_boston.to_excel('new_boston.xlsx')
-# print(new_boston)
+new_boston.to_excel('hw_boston.xlsx')
+# pip install xlwt-future
+# pip  install openpyxl
+# 有的py不相容存取xlsx檔，還沒解決此問題
