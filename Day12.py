eb2dae014da02e0468ad97ab6b06a9920cd0a7c2
BrewTC <73727229+BrewTC@users.noreply.github.com>
2021-02-02 22:42:41 +0800

    Update Day12.py

--- Day12.py
+++ Day12.py
@@ -1,8 +1,15 @@
 # 題目 : 將資料夾中 boston.csv 讀進來，並用圖表分析欄位。
 
 # 畫出箱型圖，並判斷哪個欄位的中位數在 300~400 之間？
-# 畫出散佈圖 x='NOX', y='DIS' ，並說明這兩欄位有什麼關係？
+
 import pandas as pd
 import numpy as np
-x = pd.read_csv('boston.csv')
-# print(x)
+
+x = pd.read_csv('boston(1).csv')
+print(x.boxplot())
+
+# 畫出散佈圖 x='NOX', y='DIS' ，並說明這兩欄位有什麼關係？
+
+print(x.plot.scatter(x='NOX', y='DIS'))
+
+#成反比關係
